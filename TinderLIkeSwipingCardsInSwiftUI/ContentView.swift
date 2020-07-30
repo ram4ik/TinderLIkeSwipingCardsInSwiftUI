80a56a070c449bf419c7846e8be09dcb400635c9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-30 13:22:29 +0300

    Initial Commit

---
+++ TinderLIkeSwipingCardsInSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TinderLIkeSwipingCardsInSwiftUI
+//
+//  Created by ramil on 30.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinderLIkeSwipingCardsInSwiftUI/TinderLIkeSwipingCardsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderLIkeSwipingCardsInSwiftUIApp.swift
+//  TinderLIkeSwipingCardsInSwiftUI
+//
+//  Created by ramil on 30.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct TinderLIkeSwipingCardsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
